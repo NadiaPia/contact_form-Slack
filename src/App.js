28b54d5d4cdc754693fe4ceb2628764c6c4ab8c8
NadiaPia <206i417@gmail.com>
2023-10-11 18:44:49 -0700

    Add axios.pos request to the slack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import secrets from './secrets.json';
-
 
 function App() {
 
@@ -9,16 +9,39 @@ function App() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
-  const webHookURL = secrets.SECRET_URL;
 
-  function submitForm(e) {
+  async function submitForm(e) {
     e.preventDefault();
-    alert(`${name} - ${email} - ${message}`)
+    const webHookURL = secrets.SECRET_URL;
+    const data = {
+      "text": `Name: ${name}; Email: ${email}; Message: ${message}`
+    }
+    
+    //!!!!!!!!!!!!axios.post didn't work as my adBlock blocked this request. I inactivated this extantion!!!!!!!!!!!!!!!!!!!
+
+    let res = await axios.post(webHookURL, JSON.stringify(data), {
+      withCredentials: false,
+      transformRequest: [(data, headers) => { //as axios has headers in default, we need delete it
+        console.log("headers", headers)
+        delete headers["Content-Type"] //delete headers.post["Content-Type"] doesn't work
+        return data
+      }]
+    });
+
+    if(res.status === 200) {
+      alert("Message sent!");
+      setName('');
+      setEmail('');
+      setMessage('');
+    } else {
+      alert("Error sending message.Try again!")
+    };
+
 
   }
   return (
     <div className='flex'>
-      <div className='w-1'/>
+      <div className='w-1' />
       <div className='container mex-auto mt-5'>
         <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 self-center'>
           <label htmlFor='name' className='bloc text-gray-700 text-sm font-bold mt-2'>
@@ -34,7 +57,7 @@ function App() {
           />
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
-        </label>
+          </label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="email"
@@ -46,7 +69,7 @@ function App() {
 
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
             Message
-        </label>
+          </label>
           <textarea
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="message"
@@ -61,13 +84,12 @@ function App() {
             onClick={(e) => submitForm(e)}
           >
             Submit
-        </button>
+          </button>
         </form>
       </div>
     </div>
 
-
-  ) ;
+  );
 }
 
 export default App;
